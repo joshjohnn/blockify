c38a87e558ba80d64671cb2057e4e78eb01d468d
Andy Phu <andy.jiapao.phu@gmail.com>
2025-01-25 00:19:46 -0500

    styling changes to navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,12 +4,14 @@ import Link from 'next/link';
 const Navbar = () => {
     return (
       <header className="w-full flex items-center justify-between px-8 py-4 border-b border-gray-700">
-        <div className="text-green-500 text-xl font-bold">Blockify</div>
+        <div className="text-green-500 text-xl font-bold pr-4">Blockify</div>
+        
         <input
           type="text"
           placeholder="$EXAMPL"
           className="bg-gray-800 text-white text-sm px-4 py-1 rounded-lg outline-none border border-gray-700 focus:border-green-500"
         />
+        <div className="flex flex-grow"/>
         <nav className="flex gap-8 text-sm">
         <Link className="text-green-500 border-b-2 border-green-500 hover:text-green-600 focus:ring-2 focus:ring-green-500" href="/">
           Home
